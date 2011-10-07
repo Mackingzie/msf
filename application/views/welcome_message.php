f5eca1c03be177080760026adbfa63af386540da
Meiioo <Meiioo@Meiioo.local>
2011-10-07 20:27:11 +0200

    Design mall m login jquery

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,56 +1,194 @@
-
-<!DOCTYPE html>
 <html>
-<head>
-  <style>
-  div { background:#def3ca; margin:3px; width:80px; 
-  display:none; float:left; text-align:center; }
-  </style>
-  <script src="http://code.jquery.com/jquery-latest.js"></script>
-</head>
-<body>
-  
-  <button id="showr">Show Answers(Test)</button>
-  <button id="hidr">Hide Answers (Test)</button>
-  <div><p>The Answers is working! :)</p></div>
+	<head>
+	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+	<title>Modular School Forms</title>
+	<meta http-equiv="Content-Language" content="en-us" />
+
+	<meta name="author" content="Meias" />
 
 
-<?php
-echo 'edit/delete not working (need js)';
-echo '<h2>Questions & Labels</h2>';
-echo '<dl class="questions">';
-foreach ($content as $item) {
-    switch ($item['type']) {
-        case '1': echo '<dt class="question" id="'.$item['id'].'">' . ' ' . $item['question'] . '<dt><dd>'.$item['answ'].'</dd>'.form_button('edit', 'edit');
-            break;
-        case '2': echo '<dt class="question" id="'.$item['id'].'">' . ' ' . $item['question'] . '<dt><dd><ul><li>'.$item['answ1'].'(Correct)</li><li>'.$item['answ2'].'</li><li>'.$item['answ3'].'</li><li>'.$item['answ4'].'</li><li>'.$item['answ5'].'</li></dd>'.form_button('edit', 'edit');
-            break;
-        case '3': echo '<dt class="question" id="'.$item['id'].'">' . ' ' . $item['question'] . '<dt><dd><ul><li>'.$item['answ1'].'(Correct)</li><li>'.$item['answ2'].'</li><li>'.$item['answ3'].'</li><li>'.$item['answ4'].'</li><li>'.$item['answ5'].'</li></dd>'.form_button('edit', 'edit');
-            break;
-        case '4': echo '<dt class="question" id="'.$item['id'].'"><h3>' . ' ' . $item['question'] . '</h3><dt><dd></dd>'.form_button('edit', 'edit');
-            break;
-        case '5': echo '<dt class="question" id="'.$item['id'].'"><h4>' . ' ' . $item['question'] . '</h4><dt><dd></dd>'.form_button('edit', 'edit');
-            break;
-        case '6': echo '<dt class="question" id="'.$item['id'].'"><i>' . ' ' . $item['question'] . '</i><dt><dd></dd>'.form_button('edit', 'edit');
-            break;
-        case '7': echo '<dt class="question" id="'.$item['id'].'"><em>' . ' ' . $item['question'] . '</em><dt><dd></dd>'.form_button('edit', 'edit');
-            break;
-    }
+         <!-- jQuery - the core -->
+	<script src="../js/jquery-1.3.2.min.js" type="text/javascript"></script>
+	<!-- Sliding effect -->
+	<script src="../js/slide.js" type="text/javascript"></script>
+
+	<!-- Begin Stylesheets -->
+		
+                
+              <link rel="stylesheet" href="css/style.css" type="text/css" >
+              <link rel="stylesheet" href="css/slide.css" type="text/css" >
+              <link rel="stylesheet" href="css/login_style.css" type="text/css" >
+              <script type="text/javascript" >
+                  // Copyright 2006-2007 javascript-array.com
+                  type=""
+var timeout	= 500;
+var closetimer	= 0;
+var ddmenuitem	= 0;
+
+// open hidden layer
+function mopen(id)
+{
+	// cancel close timer
+	mcancelclosetime();
+
+	// close old layer
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+
+	// get new layer and show it
+	ddmenuitem = document.getElementById(id);
+	ddmenuitem.style.visibility = 'visible';
+
 }
-echo '</dl>';
-?>
-<script>
-$("#showr").click(function () {
-  $("div:eq(0)").show("fast", function () {
-    /* use callee so don't have to name the function */
-    $(this).next("div").show("fast", arguments.callee);
-  });
-});
-$("#hidr").click(function () {
-  $("div").hide(2000);
-});
+// close showed layer
+function mclose()
+{
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+}
 
-</script>
+// go close timer
+function mclosetime()
+{
+	closetimer = window.setTimeout(mclose, timeout);
+}
 
-</body>
+// cancel close timer
+function mcancelclosetime()
+{
+	if(closetimer)
+	{
+		window.clearTimeout(closetimer);
+		closetimer = null;
+	}
+}
+
+// close layer when click-out
+document.onclick = mclose;
+        </script>
+
+</head>
+
+	<body>
+            <div id="main_container">
+               
+            <!-- Panel -->
+<div id="toppanel">
+	<div id="panel">
+		<div class="content clearfix">
+			<div class="left">
+				
+				<p class="grey">
+                                    Invite Onlyf!</p>
+				
+				
+			</div>
+			<div class="left">
+				<!-- Login Form -->
+				<form class="clearfix" action="#" method="post">
+					<h1>Member Login</h1>
+					<label class="grey" for="log">Username:</label>
+					<input class="field" type="text" name="log" id="log" value="" size="23" />
+					<label class="grey" for="pwd">Password:</label>
+					<input class="field" type="password" name="pwd" id="pwd" size="23" />
+	            	<label><input name="rememberme" id="rememberme" type="checkbox" checked="checked" value="forever" /> &nbsp;Remember me</label>
+        			<div class="clear"></div>
+					<input type="submit" name="submit" value="Login" class="bt_login" />
+					<a class="lost-pwd" href="#">Lost your password?</a>
+				</form>
+			</div>
+			<div class="left right">
+				<!-- Register Form -->
+				<form action="#" method="post">
+					<h1>Not a member yet? Sign Up!</h1>
+					<label class="grey" for="signup">Username:</label>
+					<input class="field" type="text" name="signup" id="signup" value="" size="23" />
+					<label class="grey" for="email">Email:</label>
+					<input class="field" type="text" name="email" id="email" size="23" />
+					<label>A password will be e-mailed to you.</label>
+					<input type="submit" name="submit" value="Register" class="bt_register" />
+				</form>
+			</div>
+		</div>
+</div> <!-- /login -->
+
+	<!-- The tab on top -->
+	<div class="tab">
+		<ul class="login">
+			<li class="left">&nbsp;</li>
+			<li>Admin</li>
+			<li class="sep">|</li>
+			<li id="toggle">
+				<a id="open" class="open" href="#">Log In | Register</a>
+				<a id="close" style="display: none;" class="close" href="#">Close Panel</a>
+			</li>
+			<li class="right">&nbsp;</li>
+		</ul>
+	</div> <!-- / top -->
+
+</div> <!--panel -->
+            
+		<div class="container">
+
+			
+			<div class="header">
+    			
+                        </div>
+
+                    <ul id="sddm">
+    <li><a href="#"
+        onmouseover="mopen('m1')"
+        onmouseout="mclosetime()">Exams</a>
+        <div id="m1"
+            onmouseover="mcancelclosetime()"
+            onmouseout="mclosetime()">
+        <a href="#">Blog</a>
+        <a href="#">Blog</a>
+       
+        </div>
+    </li>
+    <li><a href="#"
+        onmouseover="mopen('m2')"
+        onmouseout="mclosetime()">Evaulation</a>
+        <div id="m2"
+            onmouseover="mcancelclosetime()"
+            onmouseout="mclosetime()">
+        <a href="#">Test</a>
+        
+        </div>
+    </li>
+
+    <li><a href="#"
+        onmouseover="mopen('m3')"
+        onmouseout="mclosetime()">Courses</a>
+        <div id="m3"
+            onmouseover="mcancelclosetime()"
+            onmouseout="mclosetime()">
+        <a href="Test"></a>
+        </div></li>
+      
+        <li><a href="#"
+        onmouseover="mopen('m4')"
+        onmouseout="mclosetime()">Login</a>
+        <div id="m4"
+            onmouseover="mcancelclosetime()"
+            onmouseout="mclosetime()">
+      
+
+        </div>
+    </li>
+    
+      
+        
+  
+<div style="clear:both"></div>
+
+    	
+           
+
+            <div class="footer">
+    			&copy All rights reserved by Meias & Marcus!
+    		</div>
+          </div>
+</div>
+        </body>
 </html>
+
